268258b6f47e7865017cc97530861409b4b98398
samsiu200234 <samsiu200234@yahoo.com.hk>
2021-11-17 18:17:54 +0800

    update

--- lib/Class/StockTransfer.dart
+++ lib/Class/StockTransfer.dart
@@ -5,7 +5,7 @@ enum STType{
   IN,
   OUT,
   TRANSFER,
-
+  LOSS,
 
 }
 
@@ -16,21 +16,21 @@ class StockTransfer {
   String? productName;
   String locationId;
   String? locationName;
-  String? location2ID;
+  String? location2Id;
   String? location2Name;
  STType stType;
  String get stTypeName => describeEnum(stType);
 
 
 
-  StockTransfer({this.docRef, required this.timestamp, required this.plu, this.productName , required this.locationId,  this.locationName, this.location2ID, this.location2Name, required this.stType });
+  StockTransfer({this.docRef, required this.timestamp, required this.plu, this.productName , required this.locationId,  this.locationName, this.location2Id, this.location2Name, required this.stType });
   Map<String, dynamic> get toMap => {
         'timestamp': timestamp,
         'plu': plu,
         'productName': productName,
         'locationId': locationId,
         'locationName': locationName,
-        'location2ID': location2ID,
+        'location2Id': location2Id,
         'location2Name': location2Name,
     'stType' : STType.values.indexOf(this.stType),
 
@@ -44,7 +44,7 @@ class StockTransfer {
       productName: doc.data()?['productName'],
       locationId: doc.data()?['locationId'],
       locationName: doc.data()?['locationName'],
-      location2ID: doc.data()?['location2ID'],
+      location2Id: doc.data()?['location2Id'],
       location2Name: doc.data()?['location2Name'],
       stType: STType.values.elementAt(doc.data()?['stType'] ?? 0),
     );
