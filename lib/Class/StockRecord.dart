c9ade4e987ca89b564b45c3eec6d3e7f01b907cc
Sam Siu <samsiu@Sams-Mac-mini.local>
2022-02-28 17:40:58 +0800

    update

--- lib/Class/StockRecord.dart
+++ lib/Class/StockRecord.dart
@@ -1,40 +1,39 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
-enum STType{
-  IN,
-  OUT,
-  TRANSFER,
-  LOSS,
+enum StockRecordType{
+  NEWSTOCK,   ///New buying from suppiler, can attach to PO potentially
+  RETAIL, /// sell to retail
+  WHOLESALE, /// sell to wholesale
+  ONLINE, /// sell to online
+  STOCKIN, /// stock in from shop
+TRANSFER, /// transfer to other facuilties , not include stock in
+  DISMANTLE,
+ASSMBLE,
+  LOSS, /// waste and accepted loss of qty
 
 }
 
 class StockRecord {
   DocumentReference? docRef;
   DateTime timestamp;
-  String plu;
-  String? productName;
   String locationId;
   String? locationName;
   String? location2Id;
   String? location2Name;
- STType stType;
- String get stTypeName => describeEnum(stType);
- num qty;
+  StockRecordType stockRecordType;
 
 
 
-  StockRecord({this.docRef, required this.timestamp, required this.plu, this.productName , required this.locationId,  this.locationName, this.location2Id, this.location2Name, required this.stType ,required this.qty});
+  StockRecord({this.docRef, required this.timestamp, required this.locationId,  this.locationName, this.location2Id, this.location2Name, required this.stockRecordType});
   Map<String, dynamic> get toMap => {
         'timestamp': timestamp,
-        'plu': plu,
-        'productName': productName,
+
         'locationId': locationId,
         'locationName': locationName,
         'location2Id': location2Id,
         'location2Name': location2Name,
-    'stType' : STType.values.indexOf(this.stType),
-    'qty' : qty,
+    'stockRecordType' : StockRecordType.values.indexOf(this.stockRecordType),
 
 
       };
@@ -42,14 +41,13 @@ class StockRecord {
     return StockRecord(
       docRef: doc.reference,
       timestamp: doc.data()?['timestamp']?.toDate(),
-      plu: doc.data()?['plu'],
-      productName: doc.data()?['productName'],
+
       locationId: doc.data()?['locationId'],
       locationName: doc.data()?['locationName'],
       location2Id: doc.data()?['location2Id'],
       location2Name: doc.data()?['location2Name'],
-      stType: STType.values.elementAt(doc.data()?['stType'] ?? 0),
-      qty: doc.data()?['qty'],
+      stockRecordType: StockRecordType.values.elementAt(doc.data()?['stockRecordType'] ?? 0),
+
     );
   }
 
---
+++ lib/Class/StockRecordItem.dart
@@ -0,0 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
+
+
+
+class StockRecordItem {
+  DocumentReference? docRef;
+  String plu;
+  String? productName;
+ num qty;
+
+
+
+  StockRecordItem({this.docRef,  required this.plu, this.productName ,required this.qty});
+  Map<String, dynamic> get toMap => {
+        'plu': plu,
+        'productName': productName,
+    'qty' : qty,
+
+
+      };
+  factory StockRecordItem.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return StockRecordItem(
+      docRef: doc.reference,
+      plu: doc.data()?['plu'],
+      productName: doc.data()?['productName'],
+      qty: doc.data()?['qty'],
+    );
+  }
+
+  Future<void> update() async {
+    await docRef?.update(toMap);
+  }
+}
