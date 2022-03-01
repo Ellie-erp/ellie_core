beda68d0dd1d7445b1f76a3eea96d91a0cfb2a14
samsiu200234 <samsiu200234@yahoo.com.hk>
2022-03-01 22:19:15 +0800

    update

--- lib/Class/StockRecord.dart
+++ lib/Class/StockRecord.dart
@@ -1,6 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 
+
+enum StockRecordStatus{
+  AWAIT,
+  SENT,
+
+}
+
 enum StockRecordType{
   NEWSTOCK,   ///New buying from suppiler, can attach to PO potentially
 TRANSFER, /// transfer to other facuilties , not include stock in
@@ -18,11 +25,14 @@ class StockRecord {
   String? location2Id;
   String? location2Name;
   StockRecordType stockRecordType;
-  String? refId;   /// If this is New stock, it record PO
+  StockRecordStatus stockRecordStatus;
+  String? poId;   /// If this is New stock, it record PO
+  String? poSeller;
+  String? poETA;
 
 
 
-  StockRecord({this.docRef, required this.timestamp, required this.locationId,  this.locationName, this.location2Id, this.location2Name, required this.stockRecordType, this.refId});
+  StockRecord({this.docRef, required this.timestamp, required this.locationId,  this.locationName, this.location2Id, this.location2Name, required this.stockRecordType, this.poId, this.poSeller, this.poETA, required this.stockRecordStatus});
   Map<String, dynamic> get toMap => {
         'timestamp': timestamp,
 
@@ -31,7 +41,10 @@ class StockRecord {
         'location2Id': location2Id,
         'location2Name': location2Name,
     'stockRecordType' : StockRecordType.values.indexOf(this.stockRecordType),
-    'refId' : refId,
+    'stockRecordStatus' : StockRecordStatus.values.indexOf(this.stockRecordStatus),
+    'poId' : poId,
+    'poSeller': poSeller,
+    'poETA' : poETA,
 
 
       };
@@ -39,13 +52,15 @@ class StockRecord {
     return StockRecord(
       docRef: doc.reference,
       timestamp: doc.data()?['timestamp']?.toDate(),
-
       locationId: doc.data()?['locationId'],
       locationName: doc.data()?['locationName'],
       location2Id: doc.data()?['location2Id'],
       location2Name: doc.data()?['location2Name'],
       stockRecordType: StockRecordType.values.elementAt(doc.data()?['stockRecordType'] ?? 0),
-      refId: doc.data()?['refId'],
+      stockRecordStatus: StockRecordStatus.values.elementAt(doc.data()?['stockRecordStatus'] ?? 0),
+      poId: doc.data()?['poId'],
+      poSeller: doc.data()?['poSeller'],
+      poETA: doc.data()?['poETA'],
     );
   }
 
