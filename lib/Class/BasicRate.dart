1d8b4034660b3614c841b189d8e4897a279cd5fc
Sam Siu <samsiu@Sams-Mac-mini.local>
2022-03-15 10:05:51 +0800

    update

---
+++ lib/Class/BasicRate.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:ellie_core/Class/Sales.dart';
+import 'package:flutter/foundation.dart';
+import 'package:liquidity_gallery/Class.dart';
+
+class BasicRate {
+  DocumentReference? docRef;
+  DateTime timestamp;
+  num rate;
+  SalesType salesType;
+
+
+
+
+
+  BasicRate({this.docRef,required this.timestamp,required this.rate,required this.salesType});
+  Map<String, dynamic> get toMap => {
+        'timestamp': timestamp,
+        'rate': rate,
+    'salesType': SalesType.values.indexOf(this.salesType),
+
+      };
+  factory BasicRate.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return BasicRate(
+      docRef: doc.reference,
+      timestamp: doc.data()?['timestamp']?.toDate(),
+      rate: doc.data()?['rate'],
+      salesType: SalesType.values.elementAt(doc.data()?['salesType'] ?? 0),
+
+    );
+  }
+
+  Future<void> update() async {
+    await docRef!.update(toMap);
+  }
+}
