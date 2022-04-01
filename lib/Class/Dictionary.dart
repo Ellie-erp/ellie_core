f214b2997af25dd96a37defcead55771520545bc
Sam Siu <samsiu@Sams-Mac-mini.local>
2022-04-01 10:08:31 +0800

    New Dictionary Class: for Information list and referencing

---
+++ lib/Class/Dictionary.dart
@@ -0,0 +1,74 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Dictionary {
+  DocumentReference? docRef;
+  DateTime timestamp;
+  String nameZh;
+  String nameEng;
+  String? description;
+
+
+
+
+  Dictionary({this.docRef, required this.timestamp,required this.nameZh, required this.nameEng ,this.description,});
+  Map<String, dynamic> get toMap => {
+        'timestamp': timestamp,
+        'nameZh': nameZh,
+        'nameEng': nameEng,
+        'description': description,
+
+
+      };
+  factory Dictionary.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return Dictionary(
+      docRef: doc.reference,
+      timestamp: doc.data()!['timestamp']?.toDate(),
+      nameZh: doc.data()!['nameZh'],
+      nameEng: doc.data()!['nameEng'],
+      description: doc.data()!['description']?? '',
+
+
+    );
+  }
+
+  Future<void> update() async {
+    await docRef!.update(toMap);
+  }
+}
+
+
+
+class DictionaryItem {
+  DocumentReference? docRef;
+  DateTime timestamp;
+  String nameZh;
+  String nameEng;
+  String? description;
+
+
+
+  DictionaryItem({this.docRef, required this.timestamp, required this.nameZh, required this.nameEng ,this.description,});
+  Map<String, dynamic> get toMap => {
+        'timestamp': timestamp,
+        'nameZh': nameZh,      
+        'nameEng': nameEng,
+        'description': description,
+
+
+      };
+  factory DictionaryItem.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return DictionaryItem(
+      docRef: doc.reference,
+      timestamp: doc.data()!['timestamp']?.toDate(),
+      nameZh: doc.data()!['nameZh'],
+      nameEng: doc.data()!['nameEng'],
+      description: doc.data()!['description']?? '',
+
+
+    );
+  }
+
+  Future<void> update() async {
+    await docRef!.update(toMap);
+  }
+}
