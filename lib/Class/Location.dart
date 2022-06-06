6fe1d0b5d1ff227e283834efaeb5a0faf019b871
samsiu200234 <59386259+samsiu200234@users.noreply.github.com>
2022-06-06 18:27:39 +0800

    location openHour class

--- lib/Class/Location.dart
+++ lib/Class/Location.dart
@@ -28,6 +28,7 @@ class Location {
   List? webShopList;
   num credit;
   String? orgBrandId;
+  List<OpenHour>? openHour;
 
   Location(
       {this.docRef,
@@ -45,7 +46,9 @@ class Location {
       this.webShopList,
       this.credit = 0,
       required this.createDate,
-      this.orgBrandId});
+      this.orgBrandId,
+      this.openHour,
+      });
   Map<String, dynamic> get toMap => {
         'name': name,
         'type': type,
@@ -62,6 +65,7 @@ class Location {
         'credit': credit,
         'createDate': createDate,
     'orgBrandId': orgBrandId,
+    'openHour': (openHour ?? []).map((e) => e.toMap).toList(),
       };
   factory Location.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
     return Location(
@@ -82,6 +86,7 @@ class Location {
       credit: doc.data()?['credit'] ?? 0,
       createDate: doc.data()?['createDate']?.toDate(),
       orgBrandId: doc.data()?['orgBrandId'],
+        openHour: List<OpenHour>.from((doc.data()!['openHour'] ?? []).map((e) => OpenHour.fromMap(e)).toList()),
     );
   }
 
@@ -89,3 +94,42 @@ class Location {
     await docRef!.update(toMap);
   }
 }
+
+
+
+
+
+
+class OpenHour {
+  bool isMon;
+  bool isTue;
+  bool isWed;
+  bool isThu;
+  bool isFri;
+  bool isSat;
+  bool isSun;
+
+
+  OpenHour({this.isMon=true, this.isTue=true, this.isWed=true, this.isThu=true, this.isFri=true, this.isSat=true, this.isSun=true});
+
+  Map<String, dynamic> get toMap => {
+    'isMon': isMon,
+    'isTue': isTue,
+    'isWed' : isWed,
+    'isThu' : isThu,
+    'isFri' : isFri,
+    'isSat' : isSat,
+    'isSun' : isSun,
+  };
+
+  factory OpenHour.fromMap(Map<String, dynamic> map) {
+    return OpenHour(
+      isMon: map['isMon']?? true,
+      isTue: map['isTue']?? true,
+      isWed: map['isWed']?? true,
+      isThu: map['isThu']?? true,
+      isFri: map['isFri']?? true,
+      isSat: map['isSat']?? true,
+      isSun: map['isSun']?? true,
+    );
+  }}
