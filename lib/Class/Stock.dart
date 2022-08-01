6a33a154c0554bbb5a9baa810ed5a0d88dd1b725
samsiu200234 <59386259+samsiu200234@users.noreply.github.com>
2022-08-01 23:47:16 +0800

    class update

--- lib/Class/Stock.dart
+++ lib/Class/Stock.dart
@@ -1,20 +1,27 @@
+import 'dart:core';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Stock {
+import 'StockRecord.dart';
+
+class
+Stock {
   DocumentReference? docRef;
   DateTime updateDate;
   String plu;
   Map<String, num>? stockMap;
+  List<StockLevel>? stockLevel;
 
   Stock(
       {this.docRef,
       required this.updateDate,
       required this.plu,
-      this.stockMap});
+      this.stockMap, this.stockLevel});
   Map<String, dynamic> get toMap => {
         'updateDate': updateDate,
         'plu': plu,
         'stockMap': stockMap,
+    'stockLevel': (stockLevel ?? []).map((e) => e.toMap).toList(),
       };
   factory Stock.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
     return Stock(
@@ -22,6 +29,75 @@ class Stock {
       updateDate: doc.data()!['updateDate']?.toDate(),
       plu: doc.data()!['plu'],
       stockMap: Map<String, num>.from(doc.data()!['stockMap']),
+      stockLevel: List<StockLevel>.from((doc.data()!['stockLevel'] ?? []).map((e) => StockLevel.fromMap(e)).toList()),
+    );
+  }
+
+  Future<void> update() async {
+    await docRef!.update(toMap);
+  }
+}
+
+
+
+
+
+class StockLevel {
+  String locationId;
+  int qty;
+  num weight;
+
+
+
+  StockLevel({required this.locationId, this.qty=0, this.weight=0});
+
+  Map<String, dynamic> get toMap => {
+    'locationId': locationId,
+    'qty': qty,
+    'weight' : weight,
+
+
+  };
+
+  factory StockLevel.fromMap(Map<String, dynamic> map) {
+    return StockLevel(
+      locationId: map['locationId'],
+      qty: map['qty']?? 0,
+      weight: map['weight']?? 0,
+
+    );
+  }}
+
+
+
+class StockHistory {
+  DocumentReference? docRef;
+  DateTime timestamp;
+  int qty;
+  num weight;
+  String recordId;
+  bool isReverse;
+  StockRecordType stockRecordType;
+
+  StockHistory({this.docRef,required this.timestamp, this.qty=0, this.weight=0 ,this.recordId='', this.isReverse=false,      required this.stockRecordType,});
+  Map<String, dynamic> get toMap => {
+        'timestamp': timestamp,
+        'qty': qty,
+        'weight': weight,
+        'recordId': recordId,
+    'isReverse' : isReverse,
+    'stockRecordType': StockRecordType.values.indexOf(stockRecordType),
+      };
+  factory StockHistory.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    return StockHistory(
+      docRef: doc.reference,
+      timestamp: doc.data()!['timestamp']?.toDate(),
+      qty: doc.data()!['qty']?? 0,
+      weight: doc.data()!['weight']?? 0,
+      recordId: doc.data()!['recordId'],
+      isReverse: doc.data()!['isReverse']?? false,
+      stockRecordType:
+      StockRecordType.values.elementAt(doc.data()?['stockRecordType'] ?? 0),
     );
   }
 
--- lib/Class/StockRecordItem.dart
+++ lib/Class/StockRecordItem.dart
@@ -3,22 +3,25 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class StockRecordItem {
   DocumentReference? docRef;
   String plu;
-  String? productName;
-  num qty;
+  String name;
+  int qty;
+  num weight;
 
   StockRecordItem(
-      {this.docRef, required this.plu, this.productName, required this.qty});
+      {this.docRef, required this.plu, this.name='',  this.qty=0,this.weight=0 });
   Map<String, dynamic> get toMap => {
         'plu': plu,
-        'productName': productName,
+        'name': name,
         'qty': qty,
+    'weight': weight,
       };
   factory StockRecordItem.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
     return StockRecordItem(
       docRef: doc.reference,
       plu: doc.data()?['plu'],
-      productName: doc.data()?['productName'],
-      qty: doc.data()?['qty'],
+      name: doc.data()?['name']?? '',
+      qty: doc.data()?['qty']?? 0,
+      weight: doc.data()?['weight']?? 0,
     );
   }
 
