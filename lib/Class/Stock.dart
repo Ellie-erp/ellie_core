b5561c3b15f1b31d5e062036a8233355039ed55b
samsiu200234 <59386259+samsiu200234@users.noreply.github.com>
2022-08-02 11:17:25 +0800

    update

--- lib/Class/Stock.dart
+++ lib/Class/Stock.dart
@@ -4,33 +4,32 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'StockRecord.dart';
 
-class
-Stock {
+class Stock {
   DocumentReference? docRef;
   DateTime updateDate;
   String plu;
-  Map<String, num>? stockMap;
   List<StockLevel>? stockLevel;
 
   Stock(
       {this.docRef,
       required this.updateDate,
       required this.plu,
-      this.stockMap, this.stockLevel});
+      this.stockLevel});
   Map<String, dynamic> get toMap => {
         'updateDate': updateDate,
         'plu': plu,
-        'stockMap': stockMap,
-    'stockLevel': (stockLevel ?? []).map((e) => e.toMap).toList(),
+        'stockLevel': (stockLevel ?? []).map((e) => e.toMap).toList(),
       };
   factory Stock.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
     return Stock(
-      docRef: doc.reference,
-      updateDate: doc.data()!['updateDate']?.toDate(),
-      plu: doc.data()!['plu'],
-      stockMap: Map<String, num>.from(doc.data()!['stockMap']),
-      stockLevel: List<StockLevel>.from((doc.data()!['stockLevel'] ?? []).map((e) => StockLevel.fromMap(e)).toList()),
-    );
+        docRef: doc.reference,
+        updateDate: doc.data()!['updateDate']?.toDate(),
+        plu: doc.data()!['plu'],
+        stockLevel: (doc.data()!['stockLevel'] as Map<String, dynamic>)
+            .entries
+            .map((e) =>
+                StockLevel.fromMap(e.key, e.value as Map<String, dynamic>))
+            .toList());
   }
 
   Future<void> update() async {
@@ -38,37 +37,27 @@ Stock {
   }
 }
 
-
-
-
-
 class StockLevel {
   String locationId;
   int qty;
   num weight;
 
-
-
-  StockLevel({required this.locationId, this.qty=0, this.weight=0});
+  StockLevel({required this.locationId, this.qty = 0, this.weight = 0});
 
   Map<String, dynamic> get toMap => {
-    'locationId': locationId,
-    'qty': qty,
-    'weight' : weight,
+        'locationId': locationId,
+        'qty': qty,
+        'weight': weight,
+      };
 
-
-  };
-
-  factory StockLevel.fromMap(Map<String, dynamic> map) {
+  factory StockLevel.fromMap(String locationId, Map<String, dynamic> map) {
     return StockLevel(
-      locationId: map['locationId'],
-      qty: map['qty']?? 0,
-      weight: map['weight']?? 0,
-
+      locationId: locationId,
+      qty: map['qty'] ?? 0,
+      weight: map['weight'] ?? 0,
     );
-  }}
-
-
+  }
+}
 
 class StockHistory {
   DocumentReference? docRef;
@@ -79,25 +68,33 @@ class StockHistory {
   bool isReverse;
   StockRecordType stockRecordType;
 
-  StockHistory({this.docRef,required this.timestamp, this.qty=0, this.weight=0 ,this.recordId='', this.isReverse=false,      required this.stockRecordType,});
+  StockHistory({
+    this.docRef,
+    required this.timestamp,
+    this.qty = 0,
+    this.weight = 0,
+    this.recordId = '',
+    this.isReverse = false,
+    required this.stockRecordType,
+  });
   Map<String, dynamic> get toMap => {
         'timestamp': timestamp,
         'qty': qty,
         'weight': weight,
         'recordId': recordId,
-    'isReverse' : isReverse,
-    'stockRecordType': StockRecordType.values.indexOf(stockRecordType),
+        'isReverse': isReverse,
+        'stockRecordType': StockRecordType.values.indexOf(stockRecordType),
       };
   factory StockHistory.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
     return StockHistory(
       docRef: doc.reference,
       timestamp: doc.data()!['timestamp']?.toDate(),
-      qty: doc.data()!['qty']?? 0,
-      weight: doc.data()!['weight']?? 0,
+      qty: doc.data()!['qty'] ?? 0,
+      weight: doc.data()!['weight'] ?? 0,
       recordId: doc.data()!['recordId'],
-      isReverse: doc.data()!['isReverse']?? false,
+      isReverse: doc.data()!['isReverse'] ?? false,
       stockRecordType:
-      StockRecordType.values.elementAt(doc.data()?['stockRecordType'] ?? 0),
+          StockRecordType.values.elementAt(doc.data()?['stockRecordType'] ?? 0),
     );
   }
 
